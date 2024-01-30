8c74e429720e49dd71c3c718064dd050f414f0c2
chaymae <chaimaechaire@gmail.com>
2024-01-30 13:00:35 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import Chemin from './RouteProduits/Chemin';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+    <Chemin/>
+  </>
+);
+
+
+
+
+
+
